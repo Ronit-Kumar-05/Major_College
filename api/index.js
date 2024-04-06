6e460930c22b9a44561a7fc3adf729d9e97532b1
Ronit-Kumar-05 <2005686@kiit.ac.in>
2024-04-06 10:29:22 +0530

    misc

--- api/index.js
+++ api/index.js
@@ -2,13 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Mongo is connected");
+    console.log("MongoDB is connected");
   })
   .catch((err) => {
     console.log(err);
@@ -16,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is Running man on 3000");
 });
 
 app.use("/api/user", userRoutes);
+app.use("./api/auth", authRoutes);
