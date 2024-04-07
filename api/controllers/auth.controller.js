753c0ce5d816cc3f6ecacdac989fae8c28d6883e
Ronit-Kumar-05 <2005686@kiit.ac.in>
2024-04-07 10:39:50 +0530

    create signup api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,32 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-  console.log(req.body);
+  const { usernmae, email, password } = req.body;
+
+  if (
+    !usernmae ||
+    !email ||
+    !password ||
+    usernmae === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  const newUser = new User({
+    usernmae,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json("SIGNUP SUCCESS");
+  } catch (error) {
+    res.status(500).json({ message: "ERROR" });
+  }
 };
